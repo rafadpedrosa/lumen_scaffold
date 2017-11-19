1fdf35a39e09d9998eef549bef802d8e3d9e432c
Rafael Dias Pedrosa <rafadpedrosa@gmail.com>
2017-11-19 21:38:00 +0000

    scaffold flow done; next tep is organize the template to be funcional

--- src/LumenScaffoldCommand.php
+++ src/LumenScaffoldCommand.php
@@ -7,6 +7,13 @@ use Illuminate\Filesystem\Filesystem;
 
 class LumenScaffoldCommand extends Command
 {
+    //init variables
+    private $templates_dir;
+    private $tmp_dir;
+    private $controller_file;
+    private $model;
+    private $app_dir;
+
     /**
      * The filesystem instance.
      *
@@ -18,7 +25,7 @@ class LumenScaffoldCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'lumen_scaffold:start {--model : model name}
+    protected $signature = 'lumen_scaffold:start {model : model name}
                             {--options : options}';
 
     /**
@@ -38,6 +45,12 @@ class LumenScaffoldCommand extends Command
     {
         parent::__construct();
         $this->files = $files;
+        //init variables
+        $this->model = '';
+        $this->templates_dir = __DIR__ . '/templates/';
+        $this->tmp_dir = $this->templates_dir . 'tmp';
+        $this->controller_file = $this->tmp_dir . '/' . $this->model . 'Controler.php';
+        $this->app_dir = __DIR__ . '/../../../../app';
     }
 
     /**
@@ -50,6 +63,37 @@ class LumenScaffoldCommand extends Command
         $this->fire();
     }
 
+    private function getContentsAlreadyModifiedFromTemplateFile()
+    {
+        // Logic to read and replace file {template} names
+        // I prefer do this way to improve my php skills
+        $file = fopen($this->templates_dir . 'TemplateController.php', 'r+');
+
+        $contents = '';
+        $old = '';
+        $count = 0;
+        $fileLength = 53;
+
+        while (!feof($file)) {
+            $new = fread($file, $fileLength);
+
+            if (str_contains($old . $new, '{template}')) {
+
+                $contents = substr($contents, 0, strlen($contents) - strlen($old));
+                $increment = str_replace('{template}', $this->model, $old . $new);
+            } else {
+                $increment = $new;
+            }
+
+            $contents .= $increment;
+            $count++;
+            $old = $increment;
+        }
+        //FINISH READ
+        fclose($file);
+        return $contents;
+    }
+
     /**
      * Execute the console command.
      *
@@ -57,6 +101,24 @@ class LumenScaffoldCommand extends Command
      */
     public function fire()
     {
-        echo('FUI CHAMADO 2!');
+        $this->model = $this->argument('model');
+        try {
+
+            if (!file_exists($this->tmp_dir))
+                mkdir($this->tmp_dir);
+
+            //WRITE
+            $contents = $this->getContentsAlreadyModifiedFromTemplateFile();
+            $file = fopen($this->controller_file, 'w+');
+            fwrite($file, $contents);
+            fclose($file);
+            rename($this->controller_file, $this->app_dir . '/Http/Controllers/' . $this->model . 'Controler.php');
+        } finally {
+            rmdir($this->tmp_dir);
+        }
+//        echo $this->argument('model');
+        echo "|--------------------------------------------------------------| \n";
+        echo "|  Scaffold complete! See the new controler and we.php file!   | \n";
+        echo "|--------------------------------------------------------------| \n";
     }
 }
--- src/templates/TemplateController.php
+++ src/templates/TemplateController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-class TemplateController extends Controller
+class {template}Controller extends Controller
 {
     /**
      * Create a new controller instance.
@@ -11,34 +11,40 @@ class TemplateController extends Controller
      */
     public function __construct()
     {
-        //
+        // {template}
     }
 
     public function listAll()
     {
+        // {template}
         return response()
             ->json([0, 1, 2, 3, 4, 5]);
     }
 
     public function store($data)
     {
+        // {template}
+        var_dump('{template}');
         var_dump($data);
     }
 
     public function show($id)
     {
+        // {template}
         return response()
             ->json(["SHOW " => $id]);
     }
 
     public function update($id)
     {
+        // {template}
         return response()
             ->json(["UPDATE " => $id]);
     }
 
     public function destroy($id)
     {
+        // {template}
         return response()
             ->json(["DESTROY " => $id]);
     }
