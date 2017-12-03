c8eda7b9fe1e58c1055becf3f57943c8ddac58c1
vagrant <vagrant@homestead>
2017-12-03 16:02:14 +0000

    adding middleware to convert the resquest api to work with validate

---
+++ src/middleware/JsonApiMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace rafadpedrosa\lumen_scaffold\Middleware;
+
+use Closure;
+
+class JsonApiMiddleware
+{
+    const PARSED_METHODS = [
+        'POST', 'PUT', 'PATCH'
+    ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (in_array($request->getMethod(), self::PARSED_METHODS)) {
+            $request->merge(json_decode($request->getContent(),true));
+//            $request->merge((array)json_decode($request->getContent()));
+        }
+
+        return $next($request);
+    }
+}
